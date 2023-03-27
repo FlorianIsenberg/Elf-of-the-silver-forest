be281c91ce6bf516466567fef45eaf0227c04653
FlorianIsenberg <florianisenberg@gmail.com>
2023-03-27 13:07:47 +0200

    BackgroundUpdate

--- models/world.class.js
+++ models/world.class.js
@@ -3,7 +3,29 @@ class World {
   enemies = [new Orc(), new Orc(), new Orc()];
   backgroundObjects = [
     new BackgroundObject(
-      "./img/bg/PNG/game_background_3/layers/battleground.png"
+      "./img/bg/PNG/game_background_3/layers/battleground.png",
+      0,
+      0
+    ),
+    new BackgroundObject(
+      "./img/bg/PNG/game_background_3/layers/back_land.png",
+      0,
+      0
+    ),
+    new BackgroundObject(
+      "./img/bg/PNG/game_background_3/layers/ground_decor.png",
+      0,
+      0
+    ),
+    new BackgroundObject(
+      "./img/bg/PNG/game_background_3/layers/back_decor.png",
+      0,
+      0
+    ),
+    new BackgroundObject(
+      "./img/bg/PNG/game_background_3/layers/front_decor.png",
+      0,
+      0
     ),
   ];
   canvas;
@@ -18,14 +40,19 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addObjectsToMap(this.enemies);
     this.addToMap(this.character);
-    this.enemies.forEach((enemy) => {
-      this.addToMap(enemy);
-    });
 
     let self = this;
     requestAnimationFrame(function () {
       self.draw();
+    });
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
     });
   }
 
