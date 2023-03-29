149290bf83c72cd255688003c5e8d8ff399a8098
FlorianIsenberg <florianisenberg@gmail.com>
2023-03-29 06:26:22 +0200

    gravity

--- models/character.class.js
+++ models/character.class.js
@@ -16,6 +16,7 @@ class Character extends MovableObject {
   constructor() {
     super().loadImage("../img/elf/_PNG/3/Elf_03__WALK_000.png");
     this.loadImages(this.imagesWalkinElf);
+    this.applyGravity();
     this.walkElf();
   }
 
--- models/endboss.js
+++ models/endboss.js
@@ -12,14 +12,14 @@ class Endboss extends MovableObject {
     "../img/orc/_PNG/1_ORK/ORK_01_WALK_009.png",
   ];
   otherDirection = true;
-  height = 196.875 * 2.5;
-  width = 375 * 2.5;
+  height = 196.875 * 3.5;
+  width = 375 * 3.5;
 
   constructor() {
     super().loadImage(this.imagesWalkinBoss[0]);
     this.loadImages(this.imagesWalkinBoss);
-    this.x = 700;
-    this.y = -105;
+    this.x = 2050;
+    this.y = -270;
     this.walkEndboss();
   }
 
