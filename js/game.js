86253a41d7f6c5a8567b3118465d4d90b0f066b7
FlorianIsenberg <florianisenberg@gmail.com>
2023-04-08 21:20:09 +0200

    endscreen

--- js/game.js
+++ js/game.js
@@ -15,6 +15,8 @@ function gameStart() {
   gameSound.play();
   gameSound.loop = true;
   gameSound.volume = 0.025;
+  runGame();
+  hideScreens();
 }
 
 function setScreen() {
@@ -44,6 +46,61 @@ function setWorldAudio() {
   } else {
     world.soundMuted = false;
   }
+}
+
+function runGame() {
+  let gameInterval = setInterval(() => {
+    checkGameOver(gameInterval);
+  }, 1000 / 20);
+}
+
+function checkGameOver(gameInterval) {
+  if (world.character.energy == 0) {
+    clearInterval(gameInterval);
+    setTimeout(() => {
+      document.getElementById("endscreen").classList.remove("dNone");
+      document.getElementById("endscreenHeadline").innerHTML = "GAME OVER";
+    }, 1000);
+  }
+
+  if (world.level.endboss[0].energy == 0) {
+    clearInterval(gameInterval);
+    setTimeout(() => {
+      document.getElementById("endscreen").classList.remove("dNone");
+      document.getElementById("endscreenHeadline").innerHTML = "VICTORY";
+    }, 1000);
+  }
+}
+
+function hideScreens() {
+  document.getElementById("endscreen").classList.add("dNone");
+}
+
+function howToPlay() {
+  document.getElementById("description").innerHTML = createHowToPlay();
+}
+
+function createHowToPlay() {
+  return `
+  <div class="howToPlay">
+      <div class="walkRight">
+          Walk Right: <button class="playBtn" id="btnRight">D</button> or <button class="playBtn" id="btnRight"><img  class="arrowRight" src="img/icons/arrow-204-256.png" class="rotateRight imgSmall" alt=""></button>
+      </div>
+      <div class="walkLeft">
+          Walk Left: <button class="playBtn" id="btnRight">A</button> or <button class="playBtn" id="btnLeft"><img class="arrowLeft" src="img/icons/arrow-204-256.png" class="rotateLeft imgSmall" alt=""></button>
+      </div>
+      <div class="jump">
+          Jump: <button class="playBtnUp" id="btnRight">W</button> or <button class="playBtn" id="btnJump"><img class="arrowUp" src="img/icons/arrow-204-256.png" class="imgSmall" alt=""></button>
+      </div>
+  </div>
+  <div class="howToPlay">
+      <div class="flash">
+          <img src="img/flash/flash06.png" alt="">Flash (Requires 5 Mana): <button class="playBtnAttack" id="btnRight">SHIFT</button>
+      </div>
+      <div class="fire">
+          <img src="img/Fireballs/20.png" alt="">Fireball (Requires 30 Mana): <button class="playBtnAttack" id="btnRight">SPACE</button>
+      </div>
+  </div>`;
 }
 
 window.addEventListener("keydown", (e) => {
